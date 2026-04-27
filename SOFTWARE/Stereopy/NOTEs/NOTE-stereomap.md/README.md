1ae45de76276de2b4170283054d94b51f19647af
ydgenomics <2144752653@qq.com>
2026-04-27 17:09:19 +0800

    stereomap

---
+++ SOFTWARE/Stereopy/NOTEs/NOTE-stereomap.md/README.md
@@ -0,0 +1,4 @@
+# stereomap [html](https://www.stomics.tech/service/stereoMap_4_2/docs/index.html#stereomap)
+
+
+## 图像配准
--- SOFTWARE/Stereopy/SCRIPTs/getExprePng.py
+++ SOFTWARE/Stereopy/SCRIPTs/getExprePng.py
@@ -1,4 +1,6 @@
+# editor: yangdong
 # 260419
+# input: gef_path
 
 import stereo as st
 from scipy.sparse import coo_matrix
@@ -9,20 +11,19 @@ import sys
 import os
 
 
-raw_gef='/data/input/Files/yangdong/M.truncatula/SAW/WT202604020036551/result/Y00710F6/outs/feature_expression/Y00710F6.raw.gef'
+gef_path='/data/input/Files/yangdong/M.truncatula/SAW/WT202604020036551/result/Y00710F6/outs/feature_expression/Y00710F6.raw.gef'
+prefix = os.path.splitext(os.path.basename(gef_path))[0]  # Returns "Y00710F6.raw"
 
 
-st.io.read_gef_info(raw_gef)
+st.io.read_gef_info(gef_path)
 data = st.io.read_gef(file_path=raw_gef, bin_type='bins', bin_size=1)
 data.tl.cal_qc(); print(data)
 
 
-# 获取坐标的最大值作为矩阵维度
 max_x = data.cells_matrix['spatial'][:, 0].max() + 1
 max_y = data.cells_matrix['spatial'][:, 1].max() + 1
 
-# 构建 COO 格式的稀疏矩阵
-# 注意：coo_matrix((data, (row, col)), shape=(M, N))
+# coo_matrix((data, (row, col)), shape=(M, N))
 spatial_mtx = coo_matrix(
     (data.cells['total_counts'].values, 
     (data.cells_matrix['spatial'][:, 0], data.cells_matrix['spatial'][:, 1])),
@@ -30,4 +31,4 @@ spatial_mtx = coo_matrix(
 ); print(spatial_mtx.shape)
 spatial_mtx *= 25
 mtx = np.clip(spatial_mtx.toarray(), 0, 255)
-cv2.imwrite('raw.png', mtx)
+cv2.imwrite(prefix + '.png', mtx)
